a2916228950834a6dd062b52061635fc57ac2bf7
picxydev <35018292+picxydev@users.noreply.github.com>
2018-12-25 17:08:50 +0530

    Update sorting_objects_in_python.py

--- sorting_objects_in_python.py
+++ sorting_objects_in_python.py
@@ -1,5 +1,8 @@
 Sorting dictionary in python
 
-foo = {1:{'user': {'username':'user_a', 'date_joined':'2018-11-03'}, 'other_stuff':{}}, 2:{'user': {'username':'user_a', 'date_joined':'2018-10-03'}, 'other_stuff':{} }}
+foo = {1:{'user': {'username':'user_a', 'date_joined':'2018-11-03'}, 
+          'other_stuff':{}},
+       2:{'user': {'username':'user_a', 'date_joined':'2018-10-03'},
+          'other_stuff':{} }}
 
 sorted(foo.values(), key=lambda obj: obj['user']['date_joined'], reverse=True)
